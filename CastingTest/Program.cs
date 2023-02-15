a71afb62ef1c9eb5e01fdcd09314aaedbce1f548
cozz-py <bencozzens13@gmail.com>
2023-02-14 22:38:38 -0500

    Add project files.

---
+++ CastingTest/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int myInt = 10;
+        byte myByte = (byte)myInt;
+        Console.WriteLine("Hello, World!");
+    }
+}
