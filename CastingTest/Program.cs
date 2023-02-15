99494fc8412b69c550d6d17c4d1519e90ac6af4b
cozz-py <bencozzens13@gmail.com>
2023-02-15 17:14:40 -0500

    All lines written

--- CastingTest/Program.cs
+++ CastingTest/Program.cs
@@ -4,6 +4,21 @@
     {
         int myInt = 10;
         byte myByte = (byte)myInt;
-        Console.WriteLine("Hello, World!");
+        double myDouble = (double)myByte;
+        bool myBool = (bool)myDouble;
+        string myString = "false";
+        myBool = (bool)myString;
+        myString = (string)myInt;
+        myString = myInt.ToString();
+        myBool = (bool)myByte;
+        myByte = (byte)myBool;
+        short myShort = (short)myInt;
+        char myChar = 'x';
+        myString = (string)myChar;
+        long myLong = (long)myInt;
+        decimal myDecimal = (decimal)myLong;
+        myString = myString + myInt + myByte + myDouble + myChar;
+        Console.WriteLine(myString);
+
     }
 }
